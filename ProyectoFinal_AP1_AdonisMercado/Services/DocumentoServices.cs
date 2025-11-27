5390f0b3abe413a89e5cc65dcbbaba1aaf12f9f9
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-27 00:21:45 -0400

    Creando servicios de Documento y Vehiculo

---
+++ ProyectoFinal_AP1_AdonisMercado/Services/DocumentoServices.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_AP1_AdonisMercado.DAL;
+using ProyectoFinal_AP1_AdonisMercado.Models;
+using System.Linq.Expressions;
+
+namespace ProyectoFinal_AP1_AdonisMercado.Services;
+
+public class DocumentoServices(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Existe(int documentoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Documentos
+            .AnyAsync(d => d.DocumentoId == documentoId);
+    }
+
+    private async Task<bool> Insertar(Documento documento)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Documentos.Add(documento);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Documento documento)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Documentos.Update(documento);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Guardar(Documento documento)
+    {
+        if (!await Existe(documento.DocumentoId))
+        {
+            return await Insertar(documento);
+        }
+        else
+        {
+            return await Modificar(documento);
+        }
+    }
+
+    public async Task<Documento?> Buscar(int documentoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Documentos
+            .Include(d => d.Pedido)
+            .FirstOrDefaultAsync(d => d.DocumentoId == documentoId);
+    }
+
+    public async Task<List<Documento>> Listar(Expression<Func<Documento, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        return await contexto.Documentos
+            .Include(d => d.Pedido)
+            .Where(criterio)
+            .ToListAsync();
+    }
+
+    public async Task<bool> Eliminar(int documentoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+
+        var documento = await contexto.Documentos
+            .FirstOrDefaultAsync(d => d.DocumentoId == documentoId);
+
+        if (documento == null)
+        {
+            return false;
+        }
+
+        contexto.Documentos.Remove(documento);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+}
---
+++ ProyectoFinal_AP1_AdonisMercado/Services/VehiculoServices.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_AP1_AdonisMercado.DAL;
+using ProyectoFinal_AP1_AdonisMercado.Models;
+
+namespace ProyectoFinal_AP1_AdonisMercado.Services;
+
+public class VehiculoService(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<bool> Existe(int vehiculoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Vehiculos
+            .AnyAsync(v => v.VehiculoId == vehiculoId);
+    }
+
+    private async Task<bool> Insertar(Vehiculo vehiculo)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Vehiculos.Add(vehiculo);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Vehiculo vehiculo)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        contexto.Vehiculos.Update(vehiculo);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Guardar(Vehiculo vehiculo)
+    {
+        if (!await Existe(vehiculo.VehiculoId))
+        {
+            return await Insertar(vehiculo);
+        }
+        else
+        {
+            return await Modificar(vehiculo);
+        }
+    }
+
+    public async Task<Vehiculo?> Buscar(int vehiculoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Vehiculos
+            .Include(v => v.PedidoDetalles)
+            .FirstOrDefaultAsync(v => v.VehiculoId == vehiculoId);
+    }
+
+    public async Task<List<Vehiculo>> ListarVehiculos()
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Vehiculos
+            .Include(v => v.PedidoDetalles)
+            .ToListAsync();
+    }
+
+    public async Task<bool> Eliminar(int vehiculoId)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        var vehiculo = await Buscar(vehiculoId);
+
+        if (vehiculo != null)
+        {
+            contexto.Vehiculos.Remove(vehiculo);
+            return await contexto.SaveChangesAsync() > 0;
+        }
+
+        return false;
+    }
+}
