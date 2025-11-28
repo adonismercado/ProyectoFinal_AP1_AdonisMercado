92c70ac34aedd56f8012b3a4d97f880c910d10cf
adonismercado <adonis_mercado@ucne.edu.do>
2025-11-28 01:20:33 -0400

    Reparando base de datos

--- ProyectoFinal_AP1_AdonisMercado/Data/Migrations/20251128051319_FixDatabase.Designer.cs
+++ ProyectoFinal_AP1_AdonisMercado/Data/Migrations/20251128051319_FixDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProyectoFinal_AP1_AdonisMercado.Data;
 
@@ -11,9 +12,11 @@ using ProyectoFinal_AP1_AdonisMercado.Data;
 namespace ProyectoFinal_AP1_AdonisMercado.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251128051319_FixDatabase")]
+    partial class FixDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
