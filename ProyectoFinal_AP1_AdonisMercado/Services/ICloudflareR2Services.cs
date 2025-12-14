f0d1208ea9918a1ab3fea8caa4f021750057bba5
adonismercado <adonis_mercado@ucne.edu.do>
2025-12-14 02:03:51 -0400

    feat ✨: crear interfaz ICloudflareR2Service
    Se introdujo una interfaz para desacoplar el servicio de almacenamiento y facilitar las pruebas unitarias mediante mocks.

---
+++ ProyectoFinal_AP1_AdonisMercado/Services/ICloudflareR2Services.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace ProyectoFinal_AP1_AdonisMercado.Services;
+
+public interface ICloudflareR2Service
+{
+    Task<string?> SubirArchivo(
+        IBrowserFile archivo,
+        string carpeta,
+        string? nombrePersonalizado = null);
+
+    Task<bool> EliminarArchivo(string urlCompleta);
+}
